230371bd708b90bd6b07b1a9ecab33df704e62c5
unknown <ae5884@WCCZC51928C4.ad.mah.se>
2016-01-21 10:13:18 +0100

    I'm leaving

---
+++ Leaving.java
@@ -0,0 +1,18 @@
+package assignment2;
+
+public class Buffer {
+
+	
+	private char c;
+	
+	public void setChar(char c){
+		
+		this.c=c; 
+		System.out.println("Hej Calvin, u are fired!");
+		System.out.println("I'm leaving"); //C
+	}
+	public char getChar(){
+		return c;
+	}
+	
+}
